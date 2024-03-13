886df84c4f189da1d347abea42ceb1e922466bf4
liz(s)a <el.konovkina@mail.ru>
2024-03-13 23:08:10 +0300

    added basic encryption capability using a master password

---
+++ app/src/main/java/com/password/manager/CryptoManager.kt
@@ -0,0 +1,90 @@
+package com.password.manager
+
+import android.security.keystore.KeyProperties
+import android.security.keystore.KeyProtection
+import android.util.Base64
+import java.nio.charset.StandardCharsets
+import java.security.Key
+import java.security.KeyStore
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
+
+class CryptoManager {
+
+    private val ks = KeyStore.getInstance("AndroidKeyStore").apply {
+        load(null)
+    }
+
+    private var secretKey: Key? = null
+
+    fun checkSecretKey(alias: String): Boolean {
+        if (ks.size() == 0) {
+
+            val keyGen = KeyGenerator.getInstance(ALGORITHM)
+            keyGen.init(256)
+
+            val key: SecretKey = keyGen.generateKey()
+            val entry = KeyStore.SecretKeyEntry(key)
+            val protectionParameter =
+                KeyProtection.Builder(KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .build()
+
+            ks.setEntry(alias, entry, protectionParameter)
+            secretKey = ks.getKey(alias, null)
+
+            return true
+        } else {
+            if (ks.containsAlias(alias)) {
+                secretKey = ks.getKey(alias, null)
+
+                return true
+            }
+        }
+
+        return false
+    }
+
+    fun encrypt(plainText: String): String? {
+        return try {
+            val cipher = Cipher.getInstance(TRANSFORMATION)
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey)
+
+            val cipherText =
+                Base64.encodeToString(cipher.doFinal(plainText.toByteArray()), Base64.DEFAULT)
+            val iv = Base64.encodeToString(cipher.iv, Base64.DEFAULT)
+
+            "${cipherText}.$iv"
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    fun decrypt(cipherText: String): String? {
+        val array = cipherText.split(".")
+        val cipherData = Base64.decode(array[0], Base64.DEFAULT)
+        val iv = Base64.decode(array[1], Base64.DEFAULT)
+
+        return try {
+            val cipher = Cipher.getInstance(TRANSFORMATION)
+            val spec = IvParameterSpec(iv)
+
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, spec)
+
+            val clearText = cipher.doFinal(cipherData)
+
+            String(clearText, 0, clearText.size, StandardCharsets.UTF_8)
+        } catch (e: Exception) {
+            null
+        }
+    }
+
+    companion object {
+
+        private const val ALGORITHM = "AES"
+        private const val TRANSFORMATION = "AES/CBC/PKCS7PADDING"
+    }
+}
--- app/src/main/java/com/password/manager/MainActivity.kt
+++ app/src/main/java/com/password/manager/MainActivity.kt
@@ -3,25 +3,96 @@ package com.password.manager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.password.manager.ui.theme.PasswordmanagerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val cryptoManager = CryptoManager()
+
         setContent {
             PasswordmanagerTheme {
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    // TODO
+                    var masterPassword by remember { mutableStateOf("") }
+                    var masterPasswordEntered by remember { mutableStateOf("") }
+
+                    var password by remember { mutableStateOf("") }
+
+                    var encryptedPassword by remember { mutableStateOf("") }
+                    var decryptedPassword by remember { mutableStateOf("") }
+
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Row {
+                            TextField(value = masterPassword,
+                                onValueChange = { masterPassword = it },
+                                placeholder = { Text(text = "Master-password") })
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Button(onClick = {
+                                masterPasswordEntered =
+                                    if (cryptoManager.checkSecretKey(masterPassword)) {
+                                        "Master-password entered"
+                                    } else {
+                                        "Wrong master-password"
+                                    }
+                            }) {
+                                Text(text = "Enter")
+                            }
+                        }
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(text = masterPasswordEntered)
+
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        Row {
+                            TextField(value = password,
+                                onValueChange = { password = it },
+                                placeholder = { Text(text = "Password to encrypt") })
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Button(onClick = {
+                                cryptoManager.encrypt(password)?.let {
+                                    encryptedPassword = it
+                                }
+                                cryptoManager.decrypt(encryptedPassword)?.let {
+                                    decryptedPassword = it
+                                }
+                            }) {
+                                Text(text = "Enter")
+                            }
+                        }
+
+                        Spacer(modifier = Modifier.height(50.dp))
+
+                        Text(text = "Encrypted password:")
+                        Text(text = encryptedPassword)
+                        Spacer(modifier = Modifier.height(10.dp))
+                        Text(text = "Decrypted password: $decryptedPassword")
+                    }
                 }
             }
         }
